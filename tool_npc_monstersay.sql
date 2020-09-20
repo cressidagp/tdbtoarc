0d7f11b6905e5ecbc9570494d09ab38fd91dfb76
gcpigni <gcpigni@gmail.com>
2020-09-19 22:42:18 -0300

    Remade the whole npc_monstersay. Its exportable but needs some cleanups

--- tool_npc_monstersay.sql
+++ tool_npc_monstersay.sql
@@ -1,6 +1,6 @@
 /*
 ==============================================
-	Title: creature_text to npc_monstersay
+	Title: creature_template_addon to npc_monstersay
 	
 	From TDB: 335.20092
 	to
@@ -9,51 +9,80 @@
 */
 DROP TABLE IF EXISTS `npc_monstersay`;
 
-ALTER TABLE `creature_text` CHANGE COLUMN `CreatureID` `entry` int(10) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `creature_template_addon` CHANGE COLUMN `entry` `entry` int(10) unsigned NOT NULL DEFAULT '0';
 
--- ALTER TABLE `creature_text` DROP COLUMN `GroupID`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `event` int(10) unsigned NOT NULL DEFAULT '0' AFTER `auras`;
 
--- ALTER TABLE `creature_text` DROP COLUMN `ID`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `chance` float NOT NULL DEFAULT '0' AFTER `event`;
 
-ALTER TABLE `creature_text` ADD COLUMN `event` int(10) unsigned NOT NULL DEFAULT '0' AFTER `ID`;
+UPDATE creature_template_addon, smart_scripts
+SET creature_template_addon.chance = smart_scripts.event_chance
+WHERE (creature_template_addon.entry = smart_scripts.entryorguid AND smart_scripts.event_type = 4 AND smart_scripts.action_type = 1);
 
-ALTER TABLE `creature_text` CHANGE COLUMN `Text` `text0` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE `creature_template_addon` ADD COLUMN `language` int(10) unsigned NOT NULL DEFAULT '0' AFTER `chance`;
 
-ALTER TABLE `creature_text` ADD COLUMN `text1` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text0`;
-ALTER TABLE `creature_text` ADD COLUMN `text2` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text1`;
-ALTER TABLE `creature_text` ADD COLUMN `text3` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text2`;
-ALTER TABLE `creature_text` ADD COLUMN `text4` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text3`;
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.language = creature_text.Language
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 0);
 
-ALTER TABLE `creature_text` CHANGE COLUMN `Type` `type` int(10) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `creature_template_addon` ADD COLUMN `type` int(10) unsigned NOT NULL DEFAULT '0' AFTER `language`;
 
--- Language: leave it for now, will return to this in (A)
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.type = creature_text.Type
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 0);
 
--- Probability: leave it for now, will return to this in (B)
+ALTER TABLE `creature_template_addon` ADD COLUMN  `monstername` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `type`;
 
-ALTER TABLE `creature_text` DROP COLUMN `Emote`;
+UPDATE creature_template_addon, creature_template
+SET creature_template_addon.monstername = creature_template.name
+WHERE creature_template_addon.entry = creature_template.entry;
 
-ALTER TABLE `creature_text` DROP COLUMN `Duration`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `text0` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `monstername`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `text1` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text0`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `text2` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text1`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `text3` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text2`;
+ALTER TABLE `creature_template_addon` ADD COLUMN `text4` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci AFTER `text3`;
 
-ALTER TABLE `creature_text` DROP COLUMN `Sound`;
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.text0 = creature_text.Text
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 0);
 
-ALTER TABLE `creature_text` DROP COLUMN `BroadCastTextId`;
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.text1 = creature_text.Text
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 1);
 
-ALTER TABLE `creature_text` DROP COLUMN `TextRange`;
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.text2 = creature_text.Text
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 2);
 
-ALTER TABLE `creature_text` DROP COLUMN `comment`;
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.text3 = creature_text.Text
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 3);
 
--- Lets take care of (B):
+UPDATE creature_template_addon, creature_text
+SET creature_template_addon.text4 = creature_text.Text
+WHERE (creature_template_addon.entry = creature_text.CreatureID AND creature_text.GroupID = 0 AND creature_text.ID = 4);
 
-ALTER TABLE `creature_text` CHANGE COLUMN `Probability` `chance` int(10) unsigned NOT NULL DEFAULT '0' AFTER `event`;
+UPDATE `creature_template_addon` SET `text0` = '' WHERE `text0` IS NULL;
+UPDATE `creature_template_addon` SET `text1` = '' WHERE `text1` IS NULL;
+UPDATE `creature_template_addon` SET `text2` = '' WHERE `text2` IS NULL;
+UPDATE `creature_template_addon` SET `text3` = '' WHERE `text3` IS NULL;
+UPDATE `creature_template_addon` SET `text4` = '' WHERE `text4` IS NULL;
 
--- Lets take care of (A):
+--
+-- Lets cleanup:
+--
 
-ALTER TABLE `creature_text` CHANGE COLUMN `Language` `language` int(10) unsigned NOT NULL DEFAULT '0' AFTER `chance`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `path_id`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `mount`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `bytes1`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `bytes2`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `emote`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `visibilityDistanceType`;
+ALTER TABLE `creature_template_addon` DROP COLUMN `auras`;
 
-
--- TODO...
-
-
+--
 -- The End: rename to kickass emu way
+--
 
-RENAME TABLE `creature_template` TO `npc_monstersay`;
+RENAME TABLE `creature_template_addon` TO `npc_monstersay`;
