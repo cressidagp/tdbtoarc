a8b9255f679c3223271b709555a28b77991686e5
gcpigni <gcpigni@gmail.com>
2020-09-26 01:21:03 -0300

    After a lots of problem i finished the work on npc_gossip_textid. Warning this will be a slow one.

--- tool_npc_gossip_textid.sql
+++ tool_npc_gossip_textid.sql
@@ -13,24 +13,59 @@ DROP TABLE IF EXISTS `npc_gossip_textid`;
 -- Prepare structure
 --
 
--- TODO: DELETE FROM `gossip_menu` WHERE rowcount > 1;
+ALTER TABLE `gossip_menu` DROP PRIMARY KEY;
 
-ALTER TABLE `gossip_menu` CHANGE COLUMN `MenuID` `creatureid` int(10) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `gossip_menu` ADD COLUMN `id` int(30) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY;
 
-ALTER TABLE `gossip_menu` CHANGE COLUMN `TextID` `textid` int(10) unsigned NOT NULL DEFAULT '0';
+ALTER TABLE `gossip_menu` CHANGE COLUMN `MenuID` `creatureid` int(10) unsigned NOT NULL DEFAULT '0' AFTER `id`;
 
--- VerifiedBuild: leave it be, it wont harm
+ALTER TABLE `gossip_menu` CHANGE COLUMN `TextID` `textid` int(10) unsigned NOT NULL DEFAULT '0' AFTER `creatureid`;
+
+ALTER TABLE `gossip_menu` CHANGE COLUMN `VerifiedBuild` `VerifiedBuild` int(5) NOT NULL DEFAULT '0' AFTER `textid`;
+
+ALTER TABLE `gossip_menu` ADD COLUMN `temp_entry` int(8) unsigned NOT NULL DEFAULT '0' AFTER `VerifiedBuild`;
 
 --
--- Fill me with precious data
+-- Remove when more than one...
+--
+
+CREATE TABLE `gossip_menu2` LIKE `gossip_menu`;
+ALTER TABLE `gossip_menu2` DISABLE KEYS;
+INSERT INTO `gossip_menu2` SELECT * FROM `gossip_menu`;
+ALTER TABLE `gossip_menu2` ENABLE KEYS;
+	
+DELETE FROM `gossip_menu` WHERE `id` NOT IN (
+SELECT MAX(id)
+FROM `gossip_menu2`
+GROUP BY creatureid);	
+
+--
+-- Fill `temp_entry` with precious data then set creatureid
 --
 
 UPDATE gossip_menu, creature_template
-SET gossip_menu.creatureid = creature_template.entry
-WHERE (creature_template.gossip_menu_id != 0);
+SET gossip_menu.temp_entry = creature_template.entry
+WHERE (gossip_menu.creatureid = creature_template.gossip_menu_id);
+
+UPDATE `gossip_menu` SET `creatureid` = `temp_entry`;
 
 --
--- The End: rename to kickass emu way
+-- Remove temporary crap
+--
+
+ALTER TABLE `gossip_menu` DROP COLUMN `id`;
+ALTER TABLE `gossip_menu` DROP COLUMN `temp_entry`;
+
+DROP TABLE IF EXISTS `gossip_menu2`;
+
+--
+-- Alter table to math arcemu ways
+--
+DELETE FROM `gossip_menu` WHERE `creatureid` = 0;
+ALTER TABLE `gossip_menu` ADD PRIMARY KEY (`creatureid`);
+
+--
+-- The End: rename to kickass emu way :P
 --
 
 RENAME TABLE `gossip_menu` TO `npc_gossip_textid`;
