f668963dd95ebd99829692f036607c40586c00d3
gcpigni <gcpigni@gmail.com>
2020-09-30 01:14:06 -0300

    Added creature_names_localized

---
+++ tool_creature_names_localized.sql
@@ -0,0 +1,43 @@
+/*
+==============================================
+	Title: creature_template_locale to creature_names_localized
+	
+	From TDB: 335.20091
+	to
+	Arc: 2012-08-04_21-25_worldmap_info.sql
+==============================================
+*/
+
+DROP TABLE IF EXISTS `creature_names_localized`;
+
+--
+-- Remove Incorrect string value (im sorry China, Korea, Taiwan and Rusia)
+--
+
+DELETE FROM `creature_template_locale` WHERE `locale` = 'koKR';
+
+DELETE FROM `creature_template_locale` WHERE `locale` = 'zhCN';
+
+DELETE FROM `creature_template_locale` WHERE `locale` = 'zhTW';
+
+DELETE FROM `creature_template_locale` WHERE `locale` = 'ruRU';
+
+--
+-- Lets start!
+--
+
+ALTER TABLE `creature_template_locale` CHANGE COLUMN `entry` `id` int(30) unsigned NOT NULL;
+
+ALTER TABLE `creature_template_locale` CHANGE COLUMN `locale` `language_code` varchar(5) CHARACTER SET latin1 NOT NULL;
+
+ALTER TABLE `creature_template_locale` CHANGE COLUMN `Name` `name` varchar(100) CHARACTER SET latin1 NOT NULL;
+
+ALTER TABLE `creature_template_locale` CHANGE COLUMN `Title` `subname` varchar(100) CHARACTER SET latin1 NOT NULL;
+
+-- VerifiedBuild: leave it be, it wont harm.
+
+--
+-- The End: rename to kickass emu way
+--
+
+RENAME TABLE `creature_template_locale` TO `creature_names_localized`;
