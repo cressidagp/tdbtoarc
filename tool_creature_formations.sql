c9952ed61c78cb973ef089390271e87135629a23
gcpigni <gcpigni@gmail.com>
2020-10-17 02:11:53 -0300

    Added backup stuff and remove where leader equal to follower since they wont move in arcemu.

--- tool_creature_formations.sql
+++ tool_creature_formations.sql
@@ -2,17 +2,33 @@
 ==============================================
 	Title: creature_formations to creature_formations
 	
-	From TDB: 335.20091
+	From TDB: 335.20101
 	to
 	Arc: 2012-08-04_21-25_worldmap_info.sql
 ==============================================
 */
+
+DROP TABLE IF EXISTS `creature_formations2`;
+
+--
+-- Create a backup of original table...
+--
+
+CREATE TABLE `creature_formations2` SELECT * FROM `creature_formations`;
+
+--
+-- Drop not supported columns
+--
 
 ALTER TABLE `creature_formations` DROP COLUMN `groupAI`;
 
 ALTER TABLE `creature_formations` DROP COLUMN `point_1`;
 
 ALTER TABLE `creature_formations` DROP COLUMN `point_2`;
+
+--
+-- Here we go...
+--
 
 ALTER TABLE `creature_formations` CHANGE COLUMN `memberGUID` `spawn_id` int(10) unsigned NOT NULL DEFAULT '0';
 
@@ -21,4 +37,6 @@ ALTER TABLE `creature_formations` CHANGE COLUMN `leaderGUID` `target_spawn_id` i
 ALTER TABLE `creature_formations` CHANGE COLUMN `dist` `follow_dist` float NOT NULL DEFAULT '0';
 
 ALTER TABLE `creature_formations` CHANGE COLUMN `angle` `follow_angle` float NOT NULL DEFAULT '0' AFTER `target_spawn_id`;
+
+DELETE FROM `creature_formations` WHERE `spawn_id` = `target_spawn_id`; -- remove this or they wont move...
 
