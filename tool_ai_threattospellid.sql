acb373cdafc7de16023045db522b7509b81f72d2
gcpigni <gcpigni@gmail.com>
2020-11-18 15:33:48 -0300

    Added ai_threattospellid table.

---
+++ tool_ai_threattospellid.sql
@@ -0,0 +1,52 @@
+/*
+==============================================
+	Title: spell_threat to ai_threattospellid
+	
+	From TDB: 335.20111
+	to
+	Arc: 2012-08-04_21-25_worldmap_info.sql
+
+==============================================
+*/
+
+DROP TABLE IF EXISTS `ai_threattospellid`;
+
+--
+-- Create a backup of original table...
+--
+
+CREATE TABLE `spell_threat2` SELECT * FROM `spell_threat`;
+
+--
+-- Drop not supported columns
+--
+
+ALTER TABLE `spell_threat` DROP COLUMN `apPctMod`;
+
+--
+-- Here we go...
+--
+
+ALTER TABLE `spell_threat` CHANGE COLUMN `entry` `spell` int(11) unsigned NOT NULL DEFAULT '0';
+
+ALTER TABLE `spell_threat` CHANGE COLUMN `flatMod` `mod` int(11) NOT NULL DEFAULT '0';
+
+ALTER TABLE `spell_threat` CHANGE COLUMN `pctMod` `modcoef` float NOT NULL DEFAULT '1';
+
+--
+-- The End: rename to kickass way
+--
+
+RENAME TABLE `spell_threat` TO `ai_threattospellid`;
+
+--
+-- Rename our backup table
+--
+
+RENAME TABLE `spell_threat2` TO `spell_threat`;
+
+--
+-- Since our backup table will lost his keys we should add them again
+--
+
+ALTER TABLE `spell_threat` ADD PRIMARY KEY (`entry`);
