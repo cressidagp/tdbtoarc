6e5d173fef6a005bd66cf655146409d43c381916
cressidagp <gcpigni@gmail.com>
2020-12-23 19:44:41 -0300

    Added backup stuff to creature_quest_starter

--- scripts/tool_creature_quest_starter.sql
+++ scripts/tool_creature_quest_starter.sql
@@ -2,13 +2,19 @@
 ==============================================
 	Title: creature_queststarter to creature_quest_starter
 	
-	From TDB: 335.20091
+	From TDB: 335.20121
 	to
 	Arc: 2012-08-04_21-25_worldmap_info.sql
 ==============================================
 */
 
 DROP TABLE IF EXISTS `creature_quest_starter`;
+
+--
+-- Create a backup of original table...
+--
+
+CREATE TABLE `creature_queststarter2` SELECT * FROM `creature_queststarter`;
 
 --
 -- Lets start...
@@ -24,3 +30,14 @@ ALTER TABLE `creature_queststarter` CHANGE COLUMN `quest` `quest` int(11) unsign
 
 RENAME TABLE `creature_queststarter` TO `creature_quest_starter`;
 
+--
+-- Rename our backup table(s)
+--
+
+RENAME TABLE `creature_queststarter2` TO `creature_queststarter`;
+
+--
+-- Since our backup table(s) will lost his keys we should add them again
+--
+
+ALTER TABLE `creature_queststarter` ADD PRIMARY KEY (`id`, `quest`);
