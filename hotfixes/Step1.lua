617ec0f727e60bbe324812b7ba124403f9ccbcee
cressidagp <gcpigni@gmail.com>
2021-09-27 12:16:52 -0300

    Upgraded Step1 to work automagically (experimental).

--- hotfixes/Step1.lua
+++ hotfixes/Step1.lua
@@ -10,20 +10,53 @@
 
 --]]
 
+local displayid = WorldDBQuery( "SELECT `displayid` FROM `creature_spawns` WHERE `id` = '14'" ):GetColumn( 0 ):GetUShort();
+
 TDBTOARC = {};
 
 math.randomseed(os.time()); math.random(); math.random(); math.random();
+
+if( displayid == 0 ) 
+then
+	print("=====================================");
+	print("= TDBtoARC: appliyng hot fix step 1 =");
+	print("=====================================");
+	
+	WorldDBQuery( "ALTER TABLE `creature_spawns` CHANGE COLUMN `displayid` `displayid` int(30) NOT NULL DEFAULT '0' " );
+	
+	local result = WorldDBQuery( "SELECT `id`, `displayid` FROM `creature_spawns`" );
+	
+	if( result ~= nil )
+	then
+		repeat
+			local spid = result:GetColumn( 0 ):GetULong();
+			local disp = result:GetColumn( 1 ):GetUShort();
+			
+			if( disp == 0 )
+			then
+				local ran = math.random( -4, -1 );
+				WorldDBQuery( "UPDATE `creature_spawns` SET `displayid` = '"..ran.."' WHERE `id` = '"..spid.."' " );
+			end
+			
+		until result:NextRow() ~= true;
+		
+		print("===========================================");
+		print("= TDBtoARC: now shutdown and apply step 2 =");
+		print("===========================================");
+	end	
+end
 
 -- 1: chat #arc1 to unlock 
 -- 2: chat #arc2 to add values
 -- 3: then shutdown arcemu
 
+--[[
 function TDBTOARC.OnChatCommand( event, plr, message )
 
 	if( plr:IsGm() and message == "#arc1" )
 	then
 		WorldDBQuery( "ALTER TABLE `creature_spawns` CHANGE COLUMN `displayid` `displayid` int(30) NOT NULL DEFAULT '0' " );
-		plr:SendBroadcastMessage("Column unlocked!");
+		plr:SendBroadcastMessage( "Column unlocked!" );
 
 	elseif( plr:IsGm() and message == "#arc2" )
 	then
@@ -54,3 +87,5 @@ function TDBTOARC.OnChatCommand( event, plr, message )
 end
 
 RegisterServerHook( 16, TDBTOARC.OnChatCommand );
+
+--]]
