339dd864f7b2a1c44f6d66ecfe6e1aefa694bd40
cressidagp <gcpigni@gmail.com>
2021-10-01 19:44:13 -0300

    Install tool now can delete localizations. Closes #7

--- install.bat
+++ install.bat
@@ -17,6 +17,7 @@ SET /p world_db= World Database:
 
 SET mysqlpath=.\mysql\
 SET extrasql=.\extra\
+SET localesql=.\locale\
 
 :Begin
 CLS
@@ -25,6 +26,10 @@ ECHO.
 ECHO    C - Convert TDB to ArcEmu structure
 ECHO    R - Remove not more needed tables
 ECHO    A - Add ArcEmu world structure
+ECHO.
+ECHO.
+ECHO    L - Delete localization
+ECHO.
 ECHO.
 ECHO.
 ECHO    S - Change your settings
@@ -37,6 +42,8 @@ IF %v%==c GOTO converttdb
 IF %v%==C GOTO converttdb
 IF %v%==a GOTO addstructure
 IF %v%==A GOTO addstructure
+IF %v%==l GOTO deletelocale
+IF %v%==L GOTO deletelocale
 IF %v%==r GOTO remove
 IF %v%==R GOTO remove
 IF %v%==s GOTO top
@@ -84,10 +91,63 @@ ECHO.
 PAUSE
 GOTO begin
 
+:deletelocale
+CLS
+ECHO.
+ECHO    DE
+ECHO    ES
+ECHO    FR
+ECHO    MX
+ECHO.
+ECHO    a - Delete all localization
+ECHO.
+ECHO.
+ECHO    b - Back to main
+ECHO.
+ECHO Write name of locale
+ECHO.
+ECHO.
+SET /p del=		Name:
+ECHO.
+IF %del%==a GOTO deleteall
+IF %del%==A GOTO deleteall
+IF %del%==b GOTO begin
+IF %del%==B GOTO begin
+ECHO      Deleting...
+IF NOT EXIST "%localesql%\delete_locale_%del%.sql" GOTO error2
+ECHO.
+%mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %localesql%\delete_locale_%del%.sql
+ECHO.
+ECHO      File delete_locale_%del%.sql imported sucesfully!
+ECHO.
+PAUSE
+GOTO deletelocale
+
+:deleteall
+CLS
+ECHO.
+ECHO Delete all localization....
+ECHO.
+for %%C in (locale\*.sql) do (
+	ECHO executing: %%~nxC
+	%mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < "%%~fC"
+)
+ECHO.
+ECHO Localization deleted sucesfully!
+ECHO.
+PAUSE   
+GOTO begin
+
 :error
 ECHO	Please enter a correct character.
 ECHO.
 PAUSE
 GOTO begin
+
+:error2
+ECHO	Localization with this name not found.
+ECHO.
+PAUSE
+GOTO deletelocale
 
 :exit
---
+++ locale/delete_locale_ES.sql
@@ -0,0 +1,11 @@
+SET @LOCALE := 'esES';
+
+DELETE FROM `creature_names_localized` WHERE `language_code` = @LOCALE;
+
+DELETE FROM `gameobject_names_localized` WHERE `language_code` = @LOCALE;
+
+DELETE FROM `itempages_localized` WHERE `language_code` = @LOCALE;
+
+DELETE FROM `npc_text_localized` WHERE `language_code` = @LOCALE;
+
+DELETE FROM `quests_localized` WHERE `language_code` = @LOCALE;
